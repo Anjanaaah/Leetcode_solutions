dc4bc5b976d6fa236939bc80a823ce13869384cf
Anjana G Kumar <anjanaahkumar@gmail.com>
2026-06-04 21:20:38 +0530

    Create 27_remove_element.py

---
+++ Arrays/27_remove_element.py
@@ -0,0 +1,15 @@
+# LeetCode 27: Remove Element
+# Difficulty: Easy
+# Topic: Arrays, Two Pointers
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        
+        return j
+        
